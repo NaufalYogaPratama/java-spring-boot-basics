e8cef7bcf9578aacf37a0133334bb6e996effe46
Naufal Yoga Pratama <naufaltino2113@gmail.com>
2025-03-02 08:44:58 +0700

    feat(controller): add JdbcTemplate autowiring in MahasiswaController
    - Added JdbcTemplate autowiring in MahasiswaController

---
+++ src/main/java/com/example/controller/MahasiswaController.java
@@ -0,0 +1,19 @@
+package com.example.controller;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import com.example.model.Mahasiswa;
+
+@Controller
+public class MahasiswaController {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+}
