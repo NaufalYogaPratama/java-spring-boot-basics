8a71fa0dff6c5967bd580e25489a2afa67a5ddd8
Naufal Yoga Pratama <naufaltino2113@gmail.com>
2025-03-02 08:34:39 +0700

    initial commit

---
+++ src/main/java/com/example/kel31/Kel31Application.java
@@ -0,0 +1,13 @@
+package com.example.kel31;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kel31Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Kel31Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/kel31/Kel31ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kel31;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Kel31ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
